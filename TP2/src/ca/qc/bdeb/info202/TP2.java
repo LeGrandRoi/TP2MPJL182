606471b9cf326bf1670eb37277862adbb2f71d08
LeGrandRoi <38329401+LeGrandRoi@users.noreply.github.com>
2018-04-12 14:26:36 -0400

    Change

--- TP2/src/ca/qc/bdeb/info202/TP2.java
+++ TP2/src/ca/qc/bdeb/info202/TP2.java
@@ -15,6 +15,12 @@ public class TP2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Allo");
+        
+        System.out.println("Lol");
+        
+        System.out.println("salut");
     }
     
 }
